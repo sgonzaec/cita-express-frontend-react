88c8b2d4928139ebfd091309a459e68a820d0757
Simon Gonzalez <simongonzalezechavarria@gmail.com>
2024-01-21 22:05:47 +0100

    feat: add login component

---
+++ src/Pages/Login/Login.tsx
@@ -0,0 +1,58 @@
+import "./Login.scss";
+import Background from "../../Components/Background/Background";
+import { LoginService } from "../../Services/Login.service";
+import Loading from "../../Components/Loading/Loading";
+
+const Login = () => {
+  const { loading, register, handleSubmit, errors, onSubmit, response } =
+    LoginService();
+
+    console.log({response})
+
+  return (
+    <>
+      <Background />
+      {loading ? (
+        <Loading />
+      ) : (
+        <article className="login-container">
+          <div className="card">
+            <form onSubmit={handleSubmit(onSubmit)}>
+              <div className="input-container">
+                <label htmlFor="user"></label>
+                <input
+                  placeholder="Ingresa tu usuario"
+                  type="text"
+                  className={errors.user && "input-error"}
+                  {...register("user", { required: true })}
+                />
+                {errors.user && (
+                  <span className="input-error-text">verifica tu usuario</span>
+                )}
+              </div>
+
+              <div className="input-container">
+                <label htmlFor="password"></label>
+                <input
+                  placeholder="Ingresa tu contraseña"
+                  type="password"
+                  className={errors.password && "input-error"}
+                  {...register("password", { required: true })}
+                />
+                {errors.password && (
+                  <span className="input-error-text">
+                    verifica tu contraseña
+                  </span>
+                )}
+              </div>
+
+              <input type="submit" value="Iniciar Sesion" />
+            </form>
+          </div>
+        </article>
+      )}
+    </>
+  );
+};
+
+export default Login;
