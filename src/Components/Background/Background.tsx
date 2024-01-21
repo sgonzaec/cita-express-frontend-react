31b8cff7953b3a65bb56bb99b90c3a495fc264c2
Simon Gonzalez <simongonzalezechavarria@gmail.com>
2024-01-21 22:05:30 +0100

    feat: add component background

---
+++ src/Components/Background/Background.tsx
@@ -0,0 +1,26 @@
+import './Background.scss'
+
+const Background = () => {
+  return (
+    <>
+      <div className="background-container" >
+      <div className="bg-animated-area">
+        <ul className="bg-animated-circles">
+          <li></li>
+          <li></li>
+          <li></li>
+          <li></li>
+          <li></li>
+          <li></li>
+          <li></li>
+          <li></li>
+          <li></li>
+          <li></li>
+        </ul>
+      </div>
+      </div>
+    </>
+  );
+};
+
+export default Background;
