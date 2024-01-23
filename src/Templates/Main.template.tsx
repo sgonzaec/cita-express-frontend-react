f35e01cde7fb9dd71dde8eb944cef95aba8ebfbe
Simon Gonzalez <simongonzalezechavarria@gmail.com>
2024-01-24 00:56:16 +0100

    other: format template

--- src/Templates/Main.template.tsx
+++ src/Templates/Main.template.tsx
@@ -1,14 +1,14 @@
-import Footer from "../Components/Shared/Footer/Footer"
-import Header from "../Components/Shared/Header/Header"
+import Footer from "../Components/Shared/Footer/Footer";
+import Header from "../Components/Shared/Header/Header";
 
 const MainTemplate = ({ children }: any) => {
   return (
     <main>
-        <Header />
-        {children}
-        <Footer />
+      <Header />
+      {children}
+      <Footer />
     </main>
-  )
-}
+  );
+};
 
-export default MainTemplate
+export default MainTemplate;
