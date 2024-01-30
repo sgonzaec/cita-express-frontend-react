33eb99870e1d86e4300ff1f47a4ca8d575c0ac5f
Simon Gonzalez <simongonzalezechavarria@gmail.com>
2024-01-30 23:42:48 +0100

    feat: create compoenet

---
+++ src/Pages/Profile/Profile.tsx
@@ -0,0 +1,11 @@
+import MainTemplate from "../../Templates/Main.template"
+
+const Profile = () => {
+  return (
+    <MainTemplate>
+      <p>Profile</p>
+    </MainTemplate>
+  )
+}
+
+export default Profile
--- src/routes/routes.tsx
+++ src/routes/routes.tsx
@@ -5,6 +5,7 @@ import Register from "../Pages/Register/Register";
 import Login from "../Pages/Login/Login";
 import environments from "../Environments/environments";
 import { useAuth } from "../Context/AuthContext";
+import Profile from "../Pages/Profile/Profile";
 
 const AuthenticatedRoute: React.FC<{ element: React.ReactNode }> = ({ element }) => {
   const { isAuthenticated } = useAuth();
@@ -17,6 +18,10 @@ const routerList = createBrowserRouter(
     {
       path: `/`,
       element: <AuthenticatedRoute element={<Home />} />,
+    },
+    {
+      path: `/profile`,
+      element: <AuthenticatedRoute element={<Profile />} />,
     },
     {
       path: `/register`,
