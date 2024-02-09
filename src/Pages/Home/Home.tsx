61996a9b08e462d7bfe60f97d8e2f155f31333c3
tonguiino <santiagotq2@gmail.com>
2024-02-09 09:34:09 -0500

    feat main page content

--- src/Pages/Home/Home.tsx
+++ src/Pages/Home/Home.tsx
@@ -1,9 +1,29 @@
 import MainTemplate from "../../Templates/Main.template"
-
+import "./Home.scss";
 const Home = () => {
   return (
     <MainTemplate>
-      <p>Home</p>
+      <div className="homeContainer">
+        <div className="appointmentBox">
+          <div>
+            <ul>
+              <li>
+                <button></button>
+              </li>
+              <li>
+                <button></button>
+              </li>
+              <li>
+                <button></button>
+              </li>
+            </ul>
+          </div>
+        </div>
+        <div>
+          <h2>Cita Express somos la verga agendando</h2>
+          <p>Lorem ipsum dolor sit amet consectetur, adipisicing elit. Quod assumenda odio eos quos dolore praesentium. Corrupti neque sequi itaque, accusamus aliquid distinctio, harum sapiente quam eius sit dolores, nesciunt perspiciatis.</p>
+        </div>
+      </div>
     </MainTemplate>
   )
 }
