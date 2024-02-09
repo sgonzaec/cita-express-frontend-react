0b22e41219abd33b121ae8e6a3fbfb95c5de0ba7
tonguiino <santiagotq2@gmail.com>
2024-02-09 10:30:17 -0500

    feat create styles home

--- src/Pages/Home/Home.tsx
+++ src/Pages/Home/Home.tsx
@@ -4,24 +4,33 @@ const Home = () => {
   return (
     <MainTemplate>
       <div className="homeContainer">
-        <div className="appointmentBox">
-          <div>
-            <ul>
-              <li>
-                <button></button>
-              </li>
-              <li>
-                <button></button>
-              </li>
-              <li>
-                <button></button>
-              </li>
-            </ul>
+        <div>
+          <h2>cita Express</h2>
+        </div>
+        <div className="left">
+          <div className="appointmentBox">
+            <div className="box">
+            </div>
+            <div>
+              <ul>
+                <li>
+                  <button></button>
+                </li>
+                <li>
+                  <button></button>
+                </li>
+                <li>
+                  <button></button>
+                </li>
+              </ul>
+            </div>
           </div>
         </div>
-        <div>
-          <h2>Cita Express somos la verga agendando</h2>
-          <p>Lorem ipsum dolor sit amet consectetur, adipisicing elit. Quod assumenda odio eos quos dolore praesentium. Corrupti neque sequi itaque, accusamus aliquid distinctio, harum sapiente quam eius sit dolores, nesciunt perspiciatis.</p>
+        <div className="right">
+          <div>
+            <h2>Cita Express somos la verga agendando</h2>
+            <p>Lorem ipsum dolor sit amet consectetur, adipisicing elit. Quod assumenda odio eos quos dolore praesentium. Corrupti neque sequi itaque, accusamus aliquid distinctio, harum sapiente quam eius sit dolores, nesciunt perspiciatis.</p>
+          </div>
         </div>
       </div>
     </MainTemplate>
