8d817cef59d421ceda1200b9150aec85043d7d8c
tonguiino <santiagotq2@gmail.com>
2024-02-19 11:52:49 -0500

    fix table code

--- src/Components/Table/Tables.tsx
+++ src/Components/Table/Tables.tsx
@@ -4,39 +4,31 @@ import table from "./table.json"
 
 const Tables = () => {
 
-    return (
-<table>
-  <thead>Agendamiento</thead>
-  <tr>
-    <th>Fecha</th>
-    <th>Hora</th>
-    <th>Servicio</th>
-    <th>Tecnico</th>
-    <th>Duracion estimada</th>
-    <th>Estado cita</th>
-  </tr>
-  <tr>
-    <td>{table.map((item:any,index:any)=>{
-      return <p>{item.fecha}</p>
-    })}</td>
-    <td>{table.map((item:any,index:any)=>{
-      return <p>{item.hora}</p>
-    })}</td>
-    <td>{table.map((item:any,index:any)=>{
-      return <p>{item.servicio}</p>
-    })}</td>
-    <td>{table.map((item:any,index:any)=>{
-      return <p>{item.tecnico}</p>
-    })}</td>
-    <td>{table.map((item:any,index:any)=>{
-      return <p>{item.duracionEstimada}</p>
-    })}</td>
-    <td>{table.map((item:any,index:any)=>{
-      return <p>{item.estadoCita}</p>
-    })}</td>
-  </tr>
-</table>
-    )
-  }
-  
-  export default Tables
+  return (
+    <table>
+      <tr>
+        <th>Fecha</th>
+        <th>Hora</th>
+        <th>Servicio</th>
+        <th>Tecnico</th>
+        <th>Duracion estimada</th>
+        <th>Estado cita</th>
+      </tr>
+
+      {table.map((item: any, index: any) => {
+        return (<tr className="infoData" key={index}>
+          <td>{item.fecha}</td>
+          <td>{item.hora}</td>
+          <td>{item.tecnico}</td>
+          <td>{item.servicio}</td>
+          <td>{item.duracionEstimada}</td>
+          <td>{item.estadoCita}</td>
+        </tr>
+        )
+      })}
+
+    </table>
+  )
+}
+
+export default Tables
